9074d7e42753d07009f799a9f9704b6fce7658f7
siddhardha123 <siddhardha123123@gmail.com>
2022-09-22 22:35:00 +0530

    added new contracts

---
+++ helloworld.sol
@@ -0,0 +1,9 @@
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+contract helloWorld{
+     function hello() pure public returns(string memory){
+         return "hello world";
+     }
+
+}
